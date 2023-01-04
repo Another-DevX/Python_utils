734cd97847c816932e0b3b56716eeb29e6295a10
Lokochris <joseluismanco37@gmail.com>
2023-01-04 01:22:35 -0500

    exception fixing

--- recursive_compilation_modules.py
+++ recursive_compilation_modules.py
@@ -5,21 +5,21 @@ from recursive.fibonacci_recursive import run_fibonnaci_recursive as fibonacci
 
 if __name__ ==  '__main__':
 
-    response = int(input(
-        """
-Select a program 
---get_factorial__1 
---get_fibonacci_sequence__2 
-        """
-        ))
-    try:
-        response == 1 or response == 2
-    except:
-        raise Exception("Put a correct answer")
-    
-    if response == 1:
-        value = int(input("select a number to search factorial"))
-        factorial(value)
-    elif response == 2:
-        value = int(input("select fibonacci position"))
-        fibonacci(value)
+    def response_input():
+        response = int(input(f'Select a program \n--get_factorial__1 \n--get_fibonacci_sequence__2 '))
+        try:
+            assert response == 1 or response == 2
+            run(response)
+        except AssertionError as e:
+            print(e)
+            print("Escribe un valor valido")
+            response_input()
+
+    def run(response): 
+        if response == 1:
+            value = int(input("select a number to search factorial"))
+            factorial(value)
+        elif response == 2:
+            value = int(input("select fibonacci position"))
+            fibonacci(value)
+    response_input()
