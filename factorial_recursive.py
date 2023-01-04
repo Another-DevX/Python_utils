f09a8058b8efecdfd490783779bfd6952a91a6ba
Lokochris <joseluismanco37@gmail.com>
2023-01-03 23:26:19 -0500

    recursive_program

---
+++ factorial_recursive.py
@@ -0,0 +1,21 @@
+def get_factorial(value):
+    """get_factorial_result
+
+    Args:
+        value (INT): A positive integer number
+
+    Returns:
+        Value!: get the result of value Factorial like (n! = n * (n -1)!)
+    """
+    if value == 1:
+        return 1
+    return value * get_factorial(value - 1)
+        
+
+def run():
+    value = 5
+    print(get_factorial(value))
+        
+
+if __name__ ==  '__main__':
+    run()
